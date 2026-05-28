c06008502d738faaa07356830cae3587bbe9589d
huamanzhu123 <2123713612@qq.com>
2026-05-28 20:09:09 +0800

    leader + level

--- src/leaderboard.cpp
+++ src/leaderboard.cpp
@@ -12,3 +12,100 @@
  * 5. 保存回文件
  * 6. 提供提示与显示功能
  */
+//降序排序
+static int cmp_desc(const void* a,const void* b){
+    const LBEntry* ea =(const LBEntry*)a;
+    const LBEntry* eb =(const LBEntry*)b;
+    return eb->score - ea->score;//降序
+}
+int load_leaderboard(LBEntry arr[],int max){
+    if(!arr||max<=0) return 0;
+    FILE* fp = fopen(LEADERBOARD_FILE,"r");
+    if(!fp) return 0;
+    //如果打开成功就逐条读取数据
+    int count=0;
+    char line[MAX_LINE];
+    while(count<max && fgets(line,sizeof(line),fp)!=NULL){
+        LBEntry tmp;//临时变量
+        if (sscanf_s(line,"%31s %d",tmp.name,(unsigned)sizeof(tmp.name),&tmp.score)==2){//最多31个字符，最大设置是32，留一位存\0
+            arr[count++] = tmp;
+        }
+        else{
+            break;
+        }
+    }
+
+    fclose(fp);//关闭文件
+    if(count>1){
+        qsort(arr,count,sizeof(LBEntry),cmp_desc);
+    }
+    return count;
+}
+//更新排行榜
+void update_leaderboard(const char*name,int score){
+    if(!name || name[0]=='\0') return;
+    LBEntry arr[LEADERBOARD_SIZE];
+
+    int count = load_leaderboard(arr,LEADERBOARD_SIZE);
+    //查找是否有同名的，给found赋成对应下表
+    int found = -1;
+    for(int i=0;i<count;i++){
+        if(strncmp(arr[i].name,name,NAME_LEN)){
+            found = i;
+            break;
+        }
+    }
+    //找到同名玩家
+    if(count<LEADERBOARD_SIZE){
+        memset(&arr[count],0,sizeof(LBEntry));
+        strncpy_s(arr[count].name, NAME_LEN,name,NAME_LEN-1);
+        arr[count].score = score;
+        ++count;
+        qsort(arr,count,sizeof(LBEntry),cmp_desc);
+        save_leaderboard(arr,count);
+        return;
+
+    }
+
+}
+//写入报告
+void save_leaderboard(const LBEntry arr[], int count){
+    if(!arr || count<0) return ;
+    FILE* fp = fopen(LEADERBOARD_FILE,"w");
+    if(!fp) return;
+    for(int i=0;i<count;i++){
+        fprintf(fp,"%S %d\n",arr[i].name,arr[i].score);
+        fclose(fp);
+    }
+
+}
+//与玩家交互
+void prompt_and_update_leaderboard(int final_score){
+    char namebuf[NAME_LEN]={0};
+    printf("Game over! Your final score %d",final_score);
+    printf("Enter your name(max %d chars)",NAME_LEN-1);
+    if(fgets(namebuf,sizeof(namebuf),stdin)==NULL) return ;
+    //去除换行符
+    size_t len = strlen(namebuf);
+    if(len>0 && namebuf(len-1)=='\n'|| namebuf[len-1]=='\r'){
+        namebuf[len-1] = '\0';
+        if(len>1 && namebuf[len-2] == '\r'){
+            namebuf[len-2]='\0';
+        }
+    }
+
+    if(namebuf[0] == '\0'){
+        printf("为输入姓名，成绩未保存\n");
+        return ;
+
+    }
+
+    update_leaderboard(namebuf,final_score);
+    LBEntry arr[LEADERBOARD_SIZE];
+    int count = load_leaderboard(arr,LEADERBOARD_SIZE);
+    printf("\n==== 排行榜（前%d名) ====\n",LEADERBOARD_SIZE);
+    for(int i=0;i<count;i++){
+        printf("%2d. %-*s %5d\n",i+1,NAME_LEN-1,arr[i].name,arr[i].score);
+    }
+    printf("=================");
+}
--- src/level1.cpp
+++ src/level1.cpp
@@ -26,6 +26,62 @@
 */
 
 void level1_run(void) {
+    int score = 0;
+    int gameOver = 0;//游戏的结束标志，当这个变量为1时表示游戏结束
+    int speedMs = 200;//蛇移动的初始速度
+    int startX = WIDTH;
+    int startY = HEIGHT;
+    //初始化蛇的位置在界面的中央（高度的一半，宽度的一半）
+    int currentLevel = 1;//方便后续识别关卡
+
+    hide_cursor();//隐藏光标
+    //蛇的初始化，调用类内的方法
+    Snake s;
+    Snake_init(&s, startX,startY);//初始化位置，长度等
+    //食物类的初始化部分
+    Food f;
+    Snake snakes[1]={s};
+    place_food_safe(&f,&s,1);
+
+    clean_screen();//执行清屏的操作，准备进入循环
+    
+    while(!gameOver){
+        draw_board(&s,&f,score,speedMs);//画面的渲染
+
+        //输入与定时
+        int ticks;
+        //通过延时速度确定循环次数，保证蛇的移动速度以及相应移动按键
+        while(ticks<speedMs/20+1){
+            //检测是否有按键按下
+            process_input(&s,&gameOver);
+            if(gameOver){
+                //按“ESC”会改变gameOver的值，退出循环
+                break;
+            }
+            Sleep(20);//每次循环睡眠20ms，防止cpu占用过高
+            ticks++;
+        }
+        snake_move();//调用蛇移动的方法
+
+        //碰撞检测：碰到墙，碰到自己
+        if(check_self_collision(&s)||check_wall_collision(&s)){
+            gameOver = 1;
+            break;
+        }
+        //吃到食物检测
+        //主要通过判断蛇头的组表与食物的坐标是否重合
+        if(s.getX()==f.getX() && s.getY()==f.getY()){
+            s.snake_grow();//长度增加,新节点位置设置到蛇的尾部的位置
+            score++;
+            place_food_safe(&f,&s,1);//放置新的食物
+        }
+
+    }
+    //游戏结束之后，移动光标到画面下边，读取名字
+    set_cusor_pos(0,HEIGHT+2);
+    while(_kbhit()) _getch;//清空缓冲区
+
+    prompt_and_update_leaderboard(score);
 
 
 
